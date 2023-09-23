2fa3dd66f5484a8f9d876b51b6e8b92abb5f3331
Swapnil Gusani <swapniel99@gmail.com>
2023-09-23 15:59:40 +0530

    minor change

--- model/vanilla.py
+++ model/vanilla.py
@@ -46,15 +46,15 @@ class InputEmbeddings(nn.Embedding):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+    def __init__(self, d_model: int, max_seq_len: int, dropout: float) -> None:
         super(PositionalEncoding, self).__init__()
         self.d_model = d_model
-        self.seq_len = seq_len
+        self.seq_len = max_seq_len
         self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
-        # Create a matrix of shape [seq_len, d_model]
-        pe = torch.zeros(seq_len, d_model)
-        # Create a vector of shape (seq_len)
-        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)  # (seq_len, 1)
+        # Create a matrix of shape (max_seq_len, d_model)
+        pe = torch.zeros(max_seq_len, d_model)
+        # Create a vector of shape (max_seq_len)
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)  # (max_seq_len, 1)
         # Create a vector of shape (d_model)
         div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * (-math.log(10000.0) / d_model))  # (d_model / 2)
         # Apply sine to even indices
@@ -62,13 +62,13 @@ class PositionalEncoding(nn.Module):
         # Apply cosine to odd indices
         pe[:, 1::2] = torch.cos(position * div_term)  # cos(position * (10000 ** (2i / d_model)))
         # Add a batch dimension to the positional encoding
-        pe = pe.unsqueeze(0)  # (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)  # (1, max_seq_len, d_model)
         # Register the positional encoding as a buffer
         self.register_buffer('pe', pe)
 
     def forward(self, x):
         # (batch, seq_len, d_model)
-        x = x + self.pe[:, :x.shape[1], :].requires_grad_(False)
+        x = x + self.pe[:, :x.shape[1], :]
         return self.dropout(x)
 
 
@@ -218,7 +218,7 @@ class ProjectionLayer(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, src_vocab_size: int, tgt_vocab_size: int, max_seq_len: int, d_model: int = 512, N: int = 6,
+    def __init__(self, src_vocab_size: int, tgt_vocab_size: int, max_seq_len: int = 350, d_model: int = 512, N: int = 6,
                  h: int = 8, dropout: float = 0.1, d_ff: int = 2048) -> None:
         """
         :param src_vocab_size: Source Vocab Size
