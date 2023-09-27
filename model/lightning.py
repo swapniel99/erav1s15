cb59a1e91f6e35deceb00b304bc49724caa84677
Swapnil Gusani <swapniel99@gmail.com>
2023-09-28 02:49:27 +0530

    logging

--- model/lightning.py
+++ model/lightning.py
@@ -57,13 +57,13 @@ class Model(LightningModule):
     def training_step(self, batch, batch_idx):
         loss = self.common_step(batch)
         self.my_train_loss.update(loss, batch['label'].shape[0])
-        self.log(f"train_loss", self.my_train_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f"train_loss", self.my_train_loss.compute(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         loss = self.common_step(batch)
         self.my_val_loss.update(loss, batch['label'].shape[0])
-        self.log(f"val_loss", self.my_val_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log(f"val_loss", self.my_val_loss.compute(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
