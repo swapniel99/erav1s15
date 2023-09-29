e05d90097d00dc638b76bd139efcd40ac75a6c2a
Swapnil Gusani <swapniel99@gmail.com>
2023-09-29 22:42:59 +0530

    ffn same

--- oldmodel.py
+++ oldmodel.py
@@ -22,16 +22,16 @@ class LayerNormalization(nn.Module):
 
 
 class FeedForwardBlock(nn.Module):
-
     def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
-        super().__init__()
-        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
-        self.dropout = nn.Dropout(dropout)
-        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+        super(FeedForwardBlock, self).__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)  # w1 and b1
+        self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
+        self.linear_2 = nn.Linear(d_ff, d_model)  # w2 and b2
 
     def forward(self, x):
         # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
-        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+        return self.linear_2(self.dropout(self.linear_1(x).relu()))
+
 
 class InputEmbeddings(nn.Module):
 
