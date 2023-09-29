cfc398704c9b3c3f581234fda21a04507d105e18
Swapnil Gusani <swapniel99@gmail.com>
2023-09-29 23:41:42 +0530

    pos embedding same

--- oldmodel.py
+++ oldmodel.py
@@ -46,30 +46,33 @@ class InputEmbeddings(nn.Embedding):
 
 
 class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
-        super().__init__()
+    def __init__(self, d_model: int, max_seq_len: int, dropout: float) -> None:
+        super(PositionalEncoding, self).__init__()
         self.d_model = d_model
-        self.seq_len = seq_len
-        self.dropout = nn.Dropout(dropout)
-        # Create a matrix of shape (seq_len, d_model)
-        pe = torch.zeros(seq_len, d_model)
-        # Create a vector of shape (seq_len)
-        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
+        self.max_seq_len = max_seq_len
+        self.dropout = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
+        # Create a matrix of shape (max_seq_len, d_model)
+        pe = torch.zeros(max_seq_len, d_model)
+        # Create a vector of shape (max_seq_len)
+        position = torch.arange(0, max_seq_len, dtype=torch.float).unsqueeze(1)  # (max_seq_len, 1)
         # Create a vector of shape (d_model)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * (-math.log(10000.0) / d_model))  # (d_model / 2)
         # Apply sine to even indices
-        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * (10000 ** (2i / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)  # sin(position * (10000 ** (2i / d_model)))
         # Apply cosine to odd indices
-        pe[:, 1::2] = torch.cos(position * div_term) # cos(position * (10000 ** (2i / d_model))
+        pe[:, 1::2] = torch.cos(position * div_term)  # cos(position * (10000 ** (2i / d_model)))
         # Add a batch dimension to the positional encoding
-        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)  # (1, max_seq_len, d_model)
         # Register the positional encoding as a buffer
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq_len, d_model)
+        if x.shape[1] > self.max_seq_len:
+            raise ValueError(f"Expected sequence length {x.shape[1]} <= {self.max_seq_len} in Position Encoder.")
+        # (batch, seq_len, d_model)
+        x = x + self.pe[:, :x.shape[1], :]#.requires_grad_(False)
         return self.dropout(x)
+
 
 class ResidualConnection(nn.Module):
 
