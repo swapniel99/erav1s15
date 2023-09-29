210abfc5368e481d34a084f0174f1982cb183c20
Swapnil Gusani <swapniel99@gmail.com>
2023-09-30 01:37:31 +0530

    revert sequence sizes

--- model/lightning.py
+++ model/lightning.py
@@ -90,15 +90,15 @@ class Model(LightningModule):
             train_ds_raw, val_ds_raw = rd.split(0.9)
             self.train_ds = BilingualDataset(train_ds_raw, self.src_lang, self.tgt_lang, rd.src_tokenizer,
                                              rd.tgt_tokenizer, batch_size=self.batch_size, uniform_batches=True,
-                                             shuffle=True, max_src_len=50, src_tgt_diff=10)
+                                             shuffle=True, max_src_len=350, src_tgt_diff=350)
             self.val_ds = BilingualDataset(val_ds_raw, self.src_lang, self.tgt_lang, rd.src_tokenizer,
                                            rd.tgt_tokenizer, batch_size=self.batch_size, uniform_batches=True,
-                                           shuffle=False, max_src_len=50, src_tgt_diff=10)
+                                           shuffle=False, max_src_len=350, src_tgt_diff=350)
             del train_ds_raw, val_ds_raw
 
             self.transformer = Transformer(rd.src_tokenizer.get_vocab_size(), rd.tgt_tokenizer.get_vocab_size(),
                                            param_sharing=self.param_sharing, d_model=self.d_model, d_ff=self.d_ff,
-                                           heads=self.heads, dropout=self.dropout, max_seq_len=100)
+                                           heads=self.heads, dropout=self.dropout, max_seq_len=350)
             self.criterion = nn.CrossEntropyLoss(label_smoothing=self.label_smoothing,
                                                  ignore_index=self.train_ds.pad_token)
 
