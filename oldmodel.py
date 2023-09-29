c01fc9f45d4e74a9ba826f5f0bf7bf013b3e8dc7
Swapnil Gusani <swapniel99@gmail.com>
2023-09-29 23:30:03 +0530

    input embedding bugfix

--- oldmodel.py
+++ oldmodel.py
@@ -243,8 +243,8 @@ class Transformer(nn.Module):
 
 def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff: int=2048) -> Transformer:
     # Create the embedding layers
-    src_embed = InputEmbeddings(d_model, src_vocab_size)
-    tgt_embed = InputEmbeddings(d_model, tgt_vocab_size)
+    src_embed = InputEmbeddings(src_vocab_size, d_model)
+    tgt_embed = InputEmbeddings(tgt_vocab_size, d_model)
 
     # Create the positional encoding layers
     src_pos = PositionalEncoding(d_model, src_seq_len, dropout)
